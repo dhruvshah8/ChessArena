0d6716cdc0bc93b1454b6879e3cf3a08842a479f
Dhruv Shah <dhruvshah5784@gmail.com>
2022-01-06 17:46:32 -0500

    Initial Commit

---
+++ Chess/BoardView.swift
@@ -0,0 +1,20 @@
+//
+//  BoardView.swift
+//  Chess
+//
+//  Created by Dhruv Shah on 2022-01-06.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
