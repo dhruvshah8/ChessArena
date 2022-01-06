47a2b919fc6c306a6fe0358e41e9124e42be3840
Dhruv Shah <dhruvshah5784@gmail.com>
2022-01-06 18:26:08 -0500

    draw initial 8*8 grid

--- Chess/BoardView.swift
+++ Chess/BoardView.swift
@@ -8,13 +8,37 @@
 import UIKit
 
 class BoardView: UIView {
+    
+    let ratio: CGFloat = 0.8
+    var originX: CGFloat = 0
+    var originY: CGFloat = 0
+    var cellSide: CGFloat = 35
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        cellSide = bounds.width * ratio / 8
+        originX = bounds.width * (1-ratio)/2
+        originY = bounds.height * (1-ratio)/2
+        drawBoard()
     }
-    */
+    
+    func drawBoard()  {
+        
+        
+        for row in 0..<4 {
+            for col in 0..<4 {
+                drawSquare(col: col*2, row: row * 2, color: UIColor.white)
+                drawSquare(col: 1 + col * 2, row: row * 2, color: UIColor.lightGray)
+                drawSquare(col: col*2, row: 1 + row * 2, color: UIColor.lightGray)
+                drawSquare(col: 1 + col * 2, row: 1 + row * 2, color: UIColor.white)
+            }
+        }
+    }
+    
+    func drawSquare(col: Int, row: Int, color:UIColor) {
+        let path = UIBezierPath(rect: CGRect(x: originX + CGFloat(col) * cellSide, y: originY + CGFloat(row) * cellSide, width: cellSide, height: cellSide))
+        color.setFill()
+        path.fill()
+    }
+
 
 }
